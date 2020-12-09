6f102219a25cb2d493e9509fe51ddcdb915d2e09
Devin Eastmond <devinator9876@gmail.com>
2020-12-09 00:10:24 +0000

    started admin page

---
+++ front-end/src/views/Admin.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="admin">
+    <Header pageName="Admin"/>
+    <div class="rectangle-white form-block">
+      <h2>Admin</h2>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'Admin',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+.page-header {
+  color: rgb(0, 111, 139);
+  margin: 40px;
+}
+
+.form-block {
+  padding: 0px 40px;
+}
+
+.form-block select {
+  margin-top: 5px;
+  margin-bottom: 30px;
+  max-width: 250px;
+}
+
+.form-block h2 {
+  margin-bottom: 30px;
+}
+
+.form-block button {
+  margin: 35px 0;
+}
+
+.form-block label {
+  color: rgb(0, 111, 139);
+}
+
+.rectangle-white h2, .rectangle-white h3, .rectangle-white h4 {
+  color: rgb(0, 111, 139);
+}
+</style>
+
